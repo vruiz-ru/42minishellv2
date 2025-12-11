7685c33d7cbe725f5efdff69fc0f4b1640dbd2b9
Victor Ruiz Ruiz <vruiz-ru@c2r4s4.42madrid.com>
2025-12-11 21:50:14 +0100

    still recheable

--- input/ft_readinput.c
+++ input/ft_readinput.c
@@ -13,6 +13,7 @@
 #include "../headers/minishell.h"
 #include "input.h"
 
+/*
 void	ft_readinput(t_process *process)
 {
 	process->prompt->display = ft_prompt(process);
@@ -36,4 +37,35 @@ void	ft_readinput(t_process *process)
 		return ;
 	add_history(process->line);
 	ft_parse_line(process);
+}*/
+
+void	ft_readinput(t_process *process)
+{
+	int	exit_code;
+
+	process->prompt->display = ft_prompt(process);
+	if (process->prompt->display == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	process->line = readline(process->prompt->display);
+	if (g_signal_status != 0)
+	{
+		process->status = g_signal_status;
+		g_signal_status = 0;
+	}
+	if (process->line == NULL) // Detectamos Ctrl+D
+	{
+		ft_putstr_fd("exit\n", 1);
+		exit_code = process->status;
+		free_process(process); // <-- IMPORTANTE: Liberar memoria struct
+		rl_clear_history();    // <-- IMPORTANTE: Limpiar historial readline
+		exit(exit_code);
+	}
+	if (*(process->line) == '\0')
+		return ;
+	add_history(process->line);
+	ft_parse_line(process);
 }
+
